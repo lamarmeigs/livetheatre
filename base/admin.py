30c834819183c78ce8646e5b3473b14b394df842
Lamar Meigs <lamar.meigs@sykes.com>
2014-12-03 11:00:30 -0600

    Register models with the admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Review)
+admin.site.register(Audition)
+admin.site.register(ProductionCompany)
+admin.site.register(Production)
+admin.site.register(Play)
+admin.site.register(Venue)
+admin.site.register(Address)
+admin.site.register(ArtsNews)
+admin.site.register(Festival)
