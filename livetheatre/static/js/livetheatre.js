c1f1bb4a3974fa6e30748f3b451c0fa3b389d426
Lamar Meigs <lamar.meigs@sykes.com>
2015-01-25 08:56:25 -0600

    Bugfix on caption javascript

--- livetheatre/static/js/livetheatre.js
+++ livetheatre/static/js/livetheatre.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
 
     /* Add captions to in-body images */
-    $("#main-content img[title]").each(function() {
+    $("#main-content img[alt]").each(function() {
         caption_text = $(this).attr('alt');
         float_str = this.style.float;
         $(this)
