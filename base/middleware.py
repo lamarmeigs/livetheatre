7e164297f159f122f93c013c2eb598daaba94915
Lamar Meigs <lamar.meigs@sykes.com>
2015-02-25 10:23:34 -0600

    Install pytz, include middleware to set timezone to US/Central

---
+++ base/middleware.py
@@ -0,0 +1,8 @@
+import pytz
+from django.utils import timezone
+
+class TexasTimezoneMiddleware(object):
+    """Middleware to activate US/Central timezone, unless otherwise specified"""
+    def process_request(self, request):
+        tzname = request.session.get('timezone', 'US/Central')
+        timezone.activate(pytz.timezone(tzname))
