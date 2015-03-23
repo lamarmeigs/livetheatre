38cf0aa4368f7c5141cdeb0cc95b157a598998e8
Lamar Meigs <lamar.meigs@sykes.com>
2015-03-23 16:06:34 -0500

    Display company production's news on company detail page

--- base/views.py
+++ base/views.py
@@ -129,6 +129,17 @@ class ProductionCompanyView(DetailView):
     template_name = 'companies/detail.html'
     context_object_name = 'company'
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductionCompanyView, self).get_context_data(
+            *args, **kwargs)
+
+        # add news stories belong to this company & all its productions
+        related_news = ArtsNews.objects.filter(
+            Q(related_company=self.object) |
+            Q(related_production__production_company=self.object)).distinct()
+        context['related_news'] = related_news
+        return context
+
 
 class LocalTheatresView(ListView):
     """Display all ProductionCompany objects"""
