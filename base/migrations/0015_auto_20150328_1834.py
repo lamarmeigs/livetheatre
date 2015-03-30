b9f6025d081e64b82c578b60e0e950713a84c614
Lamar Meigs <lamar.meigs@sykes.com>
2015-03-30 18:55:51 -0500

    Make migration timezone-aware (avoids warnings during unit tests)

--- base/migrations/0015_auto_20150328_1834.py
+++ base/migrations/0015_auto_20150328_1834.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.utils.timezone import utc
 import datetime
 
 
@@ -15,13 +16,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='audition',
             name='created_on',
-            field=models.DateTimeField(default=datetime.datetime(2015, 3, 1, 0, 0), auto_now_add=True),
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 1, 0, 0).replace(tzinfo=utc), auto_now_add=True),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='production',
             name='created_on',
-            field=models.DateTimeField(default=datetime.datetime(2015, 3, 1, 0, 0), auto_now_add=True),
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 1, 0, 0).replace(tzinfo=utc), auto_now_add=True),
             preserve_default=False,
         ),
     ]
